cd48257c220b9680077bfcd7ca3b3d9a47990811
Cooper Sawyer <cooper.sawyer@powerhrg.com>
2021-09-08 09:08:50 -0400

    finished lab

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create(first_name: "Cooper", last_name: "Sawyer", grade: 99)
+Student.create(first_name: "George", last_name: "Costanza", grade: 19)
+Student.create(first_name: "Blake", last_name: "Bily", grade: 29)
+Student.create(first_name: "Copepe", last_name: "hog", grade: 89)
